a1b29fca75d5a760db30bcceccfbc8ef24a4f6a0
Jakob Murko <jakob@qy.si>
2014-02-17 10:53:51 +0100

    Fix tests...

--- tests/Databox/Tests/DataboxTestCase.php
+++ tests/Databox/Tests/DataboxTestCase.php
@@ -23,8 +23,8 @@ class DataboxTestCase extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->client = new DataboxClient('https://app.databox.com/');
-        $this->client->setApiKey('3c2pfw95lm80og0w4wgw08ccgwgo08sg');
-        $this->client->setUniqueUrl('p6a50fl7nkg84400');
+        $this->client->setApiKey('3bmuyk51ttesg888og4kcgcgs8ko8ggc');
+        $this->client->setUniqueUrl('3okcn6pedps04w8k');
         
         $adapter = new ClosureLogAdapter(function ($m) { file_put_contents('trace.log',$m,\FILE_APPEND); });
         $logPlugin = new LogPlugin($adapter, MessageFormatter::DEBUG_FORMAT);
