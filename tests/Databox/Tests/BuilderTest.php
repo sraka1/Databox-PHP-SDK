757b657479a36b4cb03e9ab4845f71579b435e20
Jakob Murko <jakob.murko@gmail.com>
2014-08-16 21:19:57 +0200

    Fix tests

--- tests/Databox/Tests/BuilderTest.php
+++ tests/Databox/Tests/BuilderTest.php
@@ -13,7 +13,7 @@ class BuilderTest extends DataboxTestCase
         $this->builder->addKpi(new KPI("milan", 150, new \DateTime("2013-09-17 23:15:18")));
         $this->builder->addKpi(new KPI("kucan", 250, new \DateTime("2013-07-30 22:55:00")));
 
-        $this->assertEquals('{"data":[{"key":"milan","value":"150","date":"2013-09-17T23:15:18","operation":null},{"key":"kucan","value":"250","date":"2013-07-30T22:55:00","operation":null}]}', $this->builder->getPayload());
+        $this->assertEquals('{"data":[{"key":"milan","value":"150","date":"2013-09-17T23:15:18","attributes":[]},{"key":"kucan","value":"250","date":"2013-07-30T22:55:00","attributes":[]}]}', $this->builder->getPayload());
     }
 
     public function testReset()
